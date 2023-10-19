057c4aaf0087af941778bea811f7fc1bef290ae1
Alex Bowes <alex.bowes@blueambertech.co.uk>
2023-10-19 10:50:44 +0100

    Add an exposed func to create a JWT

--- auth.go
+++ auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/paceperspective/auth/bajwt"
@@ -27,4 +28,9 @@ func Authorize(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+// CreateJWT creates a JWT with an additional username, uses a default expiry time of 1 hour
+func CreateJWT(ctx context.Context, userName string) (string, error) {
+	return bajwt.Create(ctx, userName, bajwt.StandardTokenLife)
 }
